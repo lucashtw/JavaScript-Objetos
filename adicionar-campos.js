d9cbe3ecc4119ab57f32747ff28d5f11b4d01b8c
lucas <lucashtw@gmail.com>
2022-05-19 12:32:43 -0300

    Objetos com javaScript

---
+++ adicionar-campos.js
@@ -0,0 +1,15 @@
+const cliente = {
+    nome: "Andre",
+    idade:36,
+    cpf:"12543652266",
+    email: "andre@email.com"
+   
+}
+
+console.log(cliente);
+
+cliente.fone = "9422525252"
+
+console.log(cliente);
+
+cliente.fone = "875254554"
